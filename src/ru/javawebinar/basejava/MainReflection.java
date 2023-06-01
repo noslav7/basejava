d48ab1366c878c3955a446fcf5ef97b8c6b5c64f
noslav7 <noslav7@gmail.com>
2023-06-01 11:00:28 +0500

    MainReflection

--- src/ru/javawebinar/basejava/MainReflection.java
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -3,10 +3,11 @@ package ru.javawebinar.basejava;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -15,6 +16,7 @@ public class MainReflection {
         field.set(r, "new_uuid");
         System.out.println(r);
         Method method = Resume.class.getMethod("toString");
+        Object result = method.invoke(r);
         System.out.println(method);
     }
 }

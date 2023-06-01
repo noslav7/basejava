d8fa2eba2e4d54b16d7ccbc371262ca76ba9df70
noslav7 <noslav7@gmail.com>
2023-06-01 11:01:09 +0500

    HW5 attempt 1 draft

---
+++ src/ru/javawebinar/basejava/storage/ListStorage.java
@@ -0,0 +1,79 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.exception.NotExistStorageException;
+import ru.javawebinar.basejava.model.Resume;
+
+import java.util.List;
+
+public class ListStorage extends AbstractStorage {
+    protected final List<Resume> resumesList;
+
+    public ListStorage(List<Resume> resumesList) {
+        this.resumesList = resumesList;
+    }
+
+    @Override
+    public void clear() {
+        resumesList.clear();
+    }
+
+    @Override
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if (index < 0) {
+            throw new NotExistStorageException(r.getUuid());
+        } else {
+            resumesList.set(index, r);
+        }
+    }
+
+    /*
+    public List<Resume> getAll() {
+        return new ArrayList<Resume>(resumesList);
+    }
+    */
+
+    @Override
+    public Resume[] getAll() {
+        return new Resume[0];
+    }
+
+    @Override
+    public int size() {
+        return resumesList.size();
+    }
+
+    @Override
+    public void save(Resume r) {
+            resumesList.add(r);
+    }
+
+    @Override
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        } else {
+            resumesList.remove(index);
+        }
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index < 0) {
+            throw new NotExistStorageException(uuid);
+        }
+        return resumesList.get(index);
+    }
+
+    @Override
+    protected int getIndex(String uuid) {
+        for (int i = 0; i < resumesList.size(); i++) {
+            if (uuid.equals(resumesList.get(i).getUuid())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}

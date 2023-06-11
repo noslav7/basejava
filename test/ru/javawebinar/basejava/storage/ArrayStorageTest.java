7be42b9392023238b546df531317bc568e64e1fe
noslav7 <noslav7@gmail.com>
2023-06-11 13:00:35 +0500

    AbstractArrayStorageTest and other small fixes

--- test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
+++ test/ru/javawebinar/basejava/storage/ArrayStorageTest.java
@@ -1,24 +1,7 @@
 package ru.javawebinar.basejava.storage;
 
-import org.junit.Assert;
-import org.junit.Test;
-import ru.javawebinar.basejava.exception.StorageException;
-import ru.javawebinar.basejava.model.Resume;
-
-public class ArrayStorageTest extends AbstractStorageTest {
+public class ArrayStorageTest extends AbstractArrayStorageTest {
     public ArrayStorageTest() {
         super(new ArrayStorage());
-    }
-
-    @Test(expected = StorageException.class)
-    public void saveOverflow() {
-        try {
-            for (int i = 4; i <= AbstractArrayStorage.STORAGE_LIMIT; i++) {
-                storage.save(new Resume("Name " + i));
-            }
-        } catch (StorageException storageException) {
-            Assert.fail("Premature StorageException");
-        }
-        storage.save(new Resume("Overflow"));
     }
 }

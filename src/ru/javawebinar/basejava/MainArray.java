24434c6ce6850e2db7b9b709666fe6b851ce4c67
noslav7 <noslav7@gmail.com>
2023-06-25 08:11:59 +0500

    -

--- src/ru/javawebinar/basejava/MainArray.java
+++ src/ru/javawebinar/basejava/MainArray.java
@@ -20,7 +20,8 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | save fullName | delete uuid | get uuid | update uuid fullName | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | save fullName | delete uuid | get uuid " +
+                    "| update uuid fullName | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 3) {
                 System.out.println("Неверная команда.");

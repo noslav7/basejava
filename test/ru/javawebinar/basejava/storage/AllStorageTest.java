b73f55d13ec4ae824f15a9fbd61fda05c41e03a6
noslav7 <noslav7@gmail.com>
2023-06-27 08:56:34 +0500

    AllStorageTest

--- test/ru/javawebinar/basejava/storage/AllStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AllStorageTest.java
@@ -11,7 +11,8 @@ import org.junit.runners.Suite;
                 ListStorageTest.class,
                 MapUuidStorageTest.class,
                 MapUuidStorageTest.class,
-                ObjectStreamStorageTest.class
+                ObjectStreamStorageTest.class,
+                ObjectStreamPathStorageTest.class
         })
 public class AllStorageTest {
 }
